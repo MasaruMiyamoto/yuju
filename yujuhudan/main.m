4452a0555193c183336f06876a0a6ab0e2fa1f16
MiyamotoMasaru <masaringaru@gmail.com>
2015-06-29 14:31:36 +0900

    Initial Commit

---
+++ yujuhudan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yujuhudan
+//
+//  Created by miyamoto masaru on 2015/06/29.
+//  Copyright (c) 2015年 com.miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
