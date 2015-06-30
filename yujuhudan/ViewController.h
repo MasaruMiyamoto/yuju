b8ea6a97b9ca3261c814327b0eb97c98739d49e8
MiyamotoMasaru <masaringaru@gmail.com>
2015-06-30 11:39:53 +0900

    2015/6/30

--- yujuhudan/ViewController.h
+++ yujuhudan/ViewController.h
@@ -8,8 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UIAlertViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UITableView *tv;
+
+@property NSMutableArray *cells;
+
+- (IBAction)insertCell:(id)sender;
+- (IBAction)deleteAll:(id)sender;
 
 @end
 
--- yujuhudan/ViewController.m
+++ yujuhudan/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.cells = [NSMutableArray array];
+    self.tv.dataSource = self;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +28,64 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)insertCell:(id)sender
+{
+    [self alertTest];
+    
+}
+
+
+- (IBAction)deleteAll:(id)sender
+{
+    self.cells = [NSMutableArray array];
+    [self.tv reloadData];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.cells count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *tvcell = [tableView dequeueReusableCellWithIdentifier:@"cid"];
+    if (tvcell == nil) {
+        tvcell   = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                          reuseIdentifier:@"cid"];
+    }
+    
+    tvcell.textLabel.text = [self.cells objectAtIndex: indexPath.row];
+    return tvcell;
+}
+
+- (void)alertTest
+{
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"入力してください" message:nil delegate:self cancelButtonTitle:@"CANCEL" otherButtonTitles:@"OK", nil];
+    [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    [message show];
+}
+
+- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView
+{
+    NSString *inputText = [[alertView textFieldAtIndex:0]text];
+    if ([inputText length] >= 1) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+        NSString *trip_title = [[alertView textFieldAtIndex:0]text];
+//        [trip_title retain];
+//        [self tripArea];
+        
+        [self.cells addObject: trip_title];
+        [self.tv reloadData];
+    }
+}
 @end
