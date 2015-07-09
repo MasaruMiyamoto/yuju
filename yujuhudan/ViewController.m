7c2dd41abd02cda051de7aea2a938724dbef9b71
MiyamotoMasaru <masaringaru@gmail.com>
2015-07-09 15:13:14 +0900

    2015/07/09  Enable for changing cells color

--- yujuhudan/ViewController.m
+++ yujuhudan/ViewController.m
@@ -43,6 +43,23 @@
 }
 
 
+//セルの更新
+- (void)updateCell:(UITableView *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    //Update Cells
+    cell.backgroundColor = [UIColor colorWithRed:0.39 green:0.73 blue:0.3 alpha:1.0];
+//    cell.backgroundColor = [UIColor colorWithRed:99 green:0 blue:76 alpha:1.0];
+//    cell.backgroundColor = [UIColor redColor];
+}
+
+- (void)updateVisibleCells
+{
+    NSLog(@"visible update");
+    for (UITableViewCell *cell in [self.tv visibleCells]){
+        [self updateCell:cell atIndexPath:[self.tv indexPathForCell:cell]];
+    }
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [self.cells count];
@@ -50,6 +67,8 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSLog(@"cellfor");
+
     UITableViewCell *tvcell = [tableView dequeueReusableCellWithIdentifier:@"cid"];
     if (tvcell == nil) {
         tvcell   = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
@@ -57,45 +76,11 @@
     }
     
     tvcell.textLabel.text = [self.cells objectAtIndex: indexPath.row];
+    
     return tvcell;
 }
 
-- (void)setAlert
-{
-    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"入力してください" message:nil delegate:self cancelButtonTitle:@"CANCEL" otherButtonTitles:@"OK", nil];
-    [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
-    [message show];
-}
-
-- (void)dicisionAlert
-{
-    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"この優柔普段\nやろうが！！" message:nil delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
-//    [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
-    [message show];
-}
-
-- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView
-{
-    NSString *inputText = [[alertView textFieldAtIndex:0]text];
-    if ([inputText length] >= 1) {
-        return YES;
-    }else{
-        return NO;
-    }
-}
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    if (buttonIndex == 1) {
-        NSString *trip_title = [[alertView textFieldAtIndex:0]text];
-//        [trip_title retain];
-//        [self tripArea];
-        
-        [self.cells addObject: trip_title];
-        [self.tv reloadData];
-    }
-}
-
+//削除可能にする
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
@@ -110,6 +95,60 @@
         
     }
 }
+
+//セルの背景色の変更
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"colorChange");
+    if(indexPath.row % 2 == 0){
+        cell.backgroundColor = [UIColor redColor];
+        //        cell.backgroundColor = [UIColor colorWithRed:99 green:188 blue:76 alpha:1.0];
+    }else{
+                cell.backgroundColor = [UIColor whiteColor];
+    }
+    //    cell.backgroundColor = self.tv.backgroundColor;
+}
+
+- (void)setAlert
+{
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"入力してください" message:nil delegate:self cancelButtonTitle:@"CANCEL" otherButtonTitles:@"OK", nil];
+    [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    [message show];
+    
+}
+
+- (void)dicisionAlert
+{
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"この優柔普段\nやろうが！！" message:nil delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+//    [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    [message show];
+}
+
+- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView
+{
+//    NSLog(@"shouldenable");
+    NSString *inputText = [[alertView textFieldAtIndex:0]text];
+    if ([inputText length] >= 1) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"clickedButton");
+    if (buttonIndex == 1) {
+        NSString *trip_title = [[alertView textFieldAtIndex:0]text];
+//        [trip_title retain];
+//        [self tripArea];
+        [self.cells addObject: trip_title];
+        [self.tv reloadData];
+        [self updateVisibleCells];
+    }
+}
+
+
 
 - (IBAction)btn:(id)sender {
     [self dicisionAlert];
